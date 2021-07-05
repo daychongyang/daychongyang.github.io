5dc3754c937f0859f17f042dab499195bac1ed71
daychongyang <daychongyang@gmail.com>
2021-07-05 10:39:26 +0800

    test: 方法语法

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
-fn main() {
-  let arr = [10, 20, 30, 40, 50];
+#[derive(Debug)]
+struct Rectangle {
+  width: u32,
+  height: u32,
+}
 
-  for it in arr.iter().rev() {
-    println!("The value is:{}", it);
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
   }
+
+  fn can_hold(&self, other: Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+
+  fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size,
+    }
+  }
+}
+
+fn main() {
+  let rect = Rectangle {
+    width: 30,
+    height: 50,
+  };
+
+  let other = Rectangle::square(29);
+
+  println!("The rect is {:#?}", rect);
+
+  println!("The area of the rectangle is {} square pixels", rect.area());
+
+  println!("Can react hold other? {}", rect.can_hold(other));
 }

333cba634e95c7086824db6aeb93bc6b46e20da3
daychongyang <daychongyang@gmail.com>
2021-07-06 10:10:37 +0800

    test: mini grep

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
-trait Summary {
-  fn summarize(&self) -> String;
-}
+use std::{env, process};
 
-struct NewsArticle {
-  pub headline: String,
-  pub location: String,
-  pub author: String,
-  pub content: String,
-}
-
-impl Summary for NewsArticle {
-  fn summarize(&self) -> String {
-    format!("{}, by {}({})", self.headline, self.author, self.location)
-  }
-}
+use daily::{run, Config};
 
 fn main() {
-  let article = NewsArticle {
-    headline: String::from("123"),
-    location: String::from("234"),
-    author: String::from("567"),
-    content: String::from("897"),
-  };
+  let args: Vec<String> = env::args().collect();
 
-  println!("1 new article: {}", article.summarize());
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    eprintln!("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
+
+  println!("Search for {}", config.query);
+  println!("In file {}", config.filename);
+
+  if let Err(e) = run(config) {
+    println!("Application error: {}", e);
+    process::exit(1);
+  }
 }
